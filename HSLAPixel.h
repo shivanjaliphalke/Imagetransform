54f0df6c7d121f7ebfeda6f8689c1b95087e66ea
Shivanjali Pratap Phalke <91719863+shivanjaliphalke@users.noreply.github.com>
2022-12-27 01:46:57 +0530

    Update HSLAPixel.h

--- HSLAPixel.h
+++ HSLAPixel.h
@@ -14,6 +14,15 @@ namespace uiuc {
 
   // Put your HSLAPixel class definition here.
   // (Remember to end it with a semicolon!)
+  class HSLAPixel {
+  public:
+  double h;
+  double s;
+  double l;
+  double a;
 
+  HSLAPixel(double h, double s, double l=1.0, double a=1.0):h(h),s(s),l(l),a(a) {}
+  HSLAPixel():HSLAPixel(1.0,1.0,1.0,1.0){}
+};
 
 }
